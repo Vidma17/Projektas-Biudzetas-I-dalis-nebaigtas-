079da3b777b772b8a758f75b1679ecbd73d48d1c
Vidma17 <vidmantas@websvetaines.eu>
2022-09-12 10:55:19 +0300

    Projektas Biudzetas I dalis

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+//        System.out.println("Hello world!");
         Programa pajamuObjektas = new Programa();
         pajamuObjektas.pajamosMetodas();
+
+        Programa islaiduObjektas = new Programa();
+        islaiduObjektas.islaidosMetodas();
     }
 }
--- src/Programa.java
+++ src/Programa.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 
 public class Programa {
     public void pajamosMetodas (){ // Metodas
-        System.out.println("Įveskite pajamas");
+        System.out.println("Įveskite pajamas:");
 
         Scanner scanner = new Scanner(System.in);
         String pajamos = scanner.nextLine();
 
         System.out.println("Sveiki, įvedėte pajamas: " + pajamos);
+    }
+
+    public void islaidosMetodas (){ // Metodas
+        System.out.println("Įveskite išlaidas:");
+
+        Scanner scanner = new Scanner(System.in);
+        String islaidos = scanner.nextLine();
+
+        System.out.println("Sveiki, įvedėte išlaidas: " + islaidos);
     }
 }
